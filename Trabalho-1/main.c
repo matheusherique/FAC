85f86020eae3f37bfa3b029de2c3916deee26f1b
Hugo Catarino <hugocatarino1@gmail.com>
2017-08-28 22:00:19 -0300

    Ajeitando main.c

--- Trabalho-1/main.c
+++ Trabalho-1/main.c
@@ -6,57 +6,10 @@
 #include "operation.h"
 
 int main() {
-<<<<<<< HEAD
   point p[3];
   circle c;
   setIn(p);
   setCenterPoint(c, p);
   c.radius = calcRadius(p[0], p[1], p[2]);
   out(c);
-=======
-  ponto p[3];
-  double distance_between_AB = 0;
-  double distance_between_BC = 0;
-  double distance_between_CA = 0;
-  double radius = 0;
-  double s = 0, u = 0, v = 0, d = 0;
-  circulo c;
-
-  int i = 0;
-  while(i < 3){
-    scanf("%lf %lf", &p[i].x, &p[i].y);
-    i++;
-  }
-
-  distance_between_AB = sqrt((p[0].x - p[1].x) * (p[0].x - p[1].x) + (p[0].y-p[1].y) *(p[0].y-p[1].y));
-  distance_between_BC = sqrt((p[1].x - p[2].x) * (p[1].x - p[2].x) + (p[1].y-p[2].y) *(p[1].y-p[2].y));
-  distance_between_CA = sqrt((p[2].x - p[0].x) * (p[2].x - p[0].x) + (p[2].y-p[0].y) *(p[2].y-p[0].y));
-
-  d = ((p[0].x - p[1].x)*(p[1].y - p[2].y)) - ((p[1].x - p[2].x)*(p[0].y - p[1].y));
-
-  s = (distance_between_AB + distance_between_BC + distance_between_CA) / 2;
-
-  radius = (distance_between_AB * distance_between_BC * distance_between_CA) /
-          (4 * sqrt(s * (s - distance_between_AB) * (s - distance_between_CA) *
-          (s - distance_between_BC)));
-
-  u = (pow(p[0].x,2) - pow(p[1].x,2) + pow(p[0].y,2) - pow(p[1].y,2)) / 2;
-  v = (pow(p[1].x,2) - pow(p[2].x,2) + pow(p[1].y,2) - pow(p[2].y,2)) / 2;
-
-  c.pontoCentro.x = (u * (p[1].y - p[2].y)- v * (p[0].y - p[1].y)) / d;
-  c.pontoCentro.y = (v * (p[0].x - p[1].x)- u * (p[1].x - p[2].x)) / d;
-  printf("radius = %lf\n", radius);
-  printf("distance_between_AB = %lf\n", distance_between_AB);
-  printf("distance_between_BC = %lf\n", distance_between_BC);
-  printf("distance_between_CA = %lf\n", distance_between_CA);
-  if(radius != radius || radius == INFINITY){
-//    printf("Circulo nao viavel.\n");
-  } else {
-    //printf("AB: %.3f, BC: %.3f, CA: %.3f\n", distance_between_AB,distance_between_BC,distance_between_CA);
-//    printf("Radius: %.3f\n", radius );
-//    printf("Centro: (%.3f, %.3f).\n",c.pontoCentro.x, c.pontoCentro.y);
-  }
->>>>>>> 050a97599e2a315e9eacbe15c837158e2d671978
-
-  return 0;
 }
